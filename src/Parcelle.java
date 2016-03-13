79116731b10d8577138ba3ce986dc3c3c01ad5f4
timothe59 <timothe.ruc@gmail.com>
2016-03-13 11:04:11 +0100

    Added files via upload

--- src/Parcelle.java
+++ src/Parcelle.java
@@ -1,66 +1,55 @@
 import java.util.ArrayList;
-
-/**
- * classe parcelle
- * @author doudou
- *
- */
-
 public class Parcelle {
+	
+	private int x;
+	private int y;
 
-	private int x, y;  // coordonnées de la parcelle; 
-	private boolean Bateau, Element, Personnage;  // boolean qui peremet de savoir la présence d'un bateau ou element ou personnage; 
+	
+	Parcelle(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	boolean equals(Parcelle parcelle){
+		if(parcelle.x == this.x && parcelle.y == this.y)
+			return true;
+		else
+			return false;
+	}
+	
+	boolean estPersonnage(Personnage personnage){ 			// comment est nommÃ© le type personnage ? j'ai mis "valeur"
+		if(personnage.valeur != 0)
+			return true;
+		else
+			return false;
+	}
+	
+	boolean estElement(Element element){
+		if(element.type != 0)								// Si le type n'est pas egal a 0, alors c'est un element
+			return true;
+		else 
+			return false;
+	}
+	
+	boolean estBateau(ArrayList <Bateau> listeBateau){		// Regarde si la parcelle est presente dans la Array ou se trouve les bateaux	
+		return( listeBateau.contains(new Parcelle(this.x,this.y)));
+	}
 	
 	public int getX() {
 		return x;
 	}
+
 	public void setX(int x) {
 		this.x = x;
 	}
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
 		this.y = y;
 	}
-	
-	//constructeur de parcelle qui prend en paramètre des coordonnées et les affectent à une parcelle;
-	public Parcelle (int x, int y){
-		
-		this.x = x;
-		this.y = y;
-			
-	}
-	//changer le SDD, passer en String et non en boolean !!
-	//méthode qui permet de savoir si l'élément est un bateau 
-	boolean EstBateau(ArrayList<Bateau> listbateau){
-		if(this.equals(listbateau.get(0).getP())||this.equals(listbateau.get(1).getP())){  //getp return la parcel de la list debateau al'indice donné
-			return true;
-	}
-		else return false;
-	}
-	
-	//renvoie le contenu en String de la parcelle à l'indice x et y donné, pour cela doit aller piocher l'indice dans la liste d'éléments
-	String contenu(int x, int y, Parcelle[][] parcelle){
-		if ( parcelle[x][y].equals(listElement.getPe()) )
-			
-		
-	}
-	
-	
-	
-	
-	//Essai perso pour comprendre l'héritage;
-	class ParcelleVide extends Parcelle {
-		
-		private String vide;
-		public ParcelleVide(int x, int y, String v){
-			super(x,y);
-			this.vide = v;
-		}
-	}
-	
-	
-	
 	
 }
